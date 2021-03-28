3d089d80c6848b1001d69970d7295ee53bdab10c
_ravin <ravindra0319@gmail.com>
2021-03-28 22:05:46 +0400

    Card vue Template/HTML Complete

--- src/components/listing/Card.vue
+++ src/components/listing/Card.vue
@@ -1,5 +1,75 @@
 <template>
-  <div class="listing-card">
+  <div class="listing-wrapper">
+
+    <div class="list-title">
+      <div class="row">
+        <div class="col-sm">
+          <div class="list-title-arrow">
+            >
+          </div>
+        </div>
+        <div class="col-sm">{{ this.title }}</div>
+        <div class="col-sm">
+          <div class="list-title-total">
+            {{ response.listing.length }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="listing-card" v-for="(list, index) in response.listing" :key="index">
+
+      <div class="card-head">
+        <div class="row">
+          <div class="col-sm">
+            <div class="card-title">REF/PO:</div>
+            <div>{{ list.load_ref }}</div>
+          </div>
+          <div class="col-sm">
+            <div class="card-title">To Pickup</div>
+            <div>{{ list.created_at }}</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm">
+            <div>Order {{ list.order_number }}</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm">
+            <div class="card-title">Created at</div>
+            <div>{{ list.created_at }}</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm">
+            <div class="card-title">Customer Name</div>
+            <div>{{ list.customer_name }}</div>
+          </div>
+          <div class="col-sm">
+            <div class="card-title">Phone Number</div>
+            <div>{{ list.phone_number }}</div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm">
+            <div class="card-title">Price to Customer</div>
+            <div>{{ list.order_price_formatted }}</div>
+          </div>
+          <div class="col-sm">
+            <div class="card-title">Tahmeel Fee</div>
+            <div>{{ list.tahmeel_fee_in_cents }}</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
 
   </div>
 </template>
@@ -36,6 +106,9 @@ export default {
       axios(this.config).then(response => {
         this.response.listing = response.data[this.id]
         this.response.loading = false
+
+        console.log("this.response.listing");
+        console.log(this.response.listing);
       }).catch(e => {
         console.log(e);
         this.response.loading = false
@@ -50,7 +123,29 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.listing-card {
+.listing-wrapper {
+  background-color: #2A2A2A;
+  border: 1px solid #0B0B0B;
 
+  .list-title {
+    color: #FFFFFF;
+  }
+
+  .listing-card {
+    .card-head {
+      background-color: #FFFFFF;
+    }
+
+    .card-body {
+      color: #FFFFFF;
+    }
+
+    .card-title {
+
+    }
+  }
 }
+// #5ADCC0
+// #545454
+// #2A2A2A
 </style>
